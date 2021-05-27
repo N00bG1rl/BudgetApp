b6e6ab73252efdc651b5d7f7e8284b6e30628ddd
N00bG1rl <marttila.helen@gmail.com>
2021-05-27 18:45:03 +0300

    Filter by year

--- src/components/budget/Budget.js
+++ src/components/budget/Budget.js
@@ -10,14 +10,17 @@ const Budget = (props) => {
   const [selectedYear, setFilteredYear] = useState('2020')
   const handleChange = selectedYear => {
     setFilteredYear(selectedYear)
-    console.log(selectedYear)
   }
+  const yearFilter = props.items.filter(result => {
+    return result.date.getFullYear().toString() === selectedYear
+  })
   return (
     <div>
       <Card className="budget">
         <FilterDate selected={ selectedYear } onChange={ handleChange } />
-        {props.items.map(item => (
+        {yearFilter.map(item => (
           <Item
+            key={item.id}
             title={item.title}
             cost={item.cost}
             date={item.date}
